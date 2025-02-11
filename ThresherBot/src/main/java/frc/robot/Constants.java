7a4e7547faf2bf60e3d58f57318d2ba8c72a430d
dravis123 <randolph.ben@gmail.com>
2025-02-11 12:25:18 -0500

    added code for wrist PID control

--- ThresherBot/src/main/java/frc/robot/Constants.java
+++ ThresherBot/src/main/java/frc/robot/Constants.java
@@ -20,23 +20,59 @@ import swervelib.math.Matter;
  * wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
+public final class Constants 
+{
    public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
    public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
    public static final double LOOP_TIME = 0.13; // s, 20ms + 110ms sprk max velocity lag
 
-   public static class DrivebaseConstants {
+   // CAN IDs HERE
+   // SWERVE ONES NOT USED IN THE CODE
+   // NEED TO UPDATE THESE LATER
+   // BackLeftDrive/Angle:   15, 10
+   // BackRightDrive/Angle:  18,  4
+   // FrontLeftDrive/Angle:  16,  7
+   // FrontRightDrive/Angle: 17,  8
+   // Pidgeon: 20
+
+   //CHANGE TO:
+   // BackLeftDrive/Angle:    4,  5
+   // BackRightDrive/Angle:   6,  7
+   // FrontLeftDrive/Angle:   8,  9
+   // FrontRightDrive/Angle: 10, 11
+   // Pidgeon: 20
+
+   public static final int ELEVATOR_MASTER_CAN_ID = 12;
+   public static final int ELEVATOR_SLAVE_CAN_ID = 13;
+   public static final int ARM_WRIST_CAN_ID = 14;
+   public static final int ARM_ENCODER_CAN_ID = 15;
+   public static final int LASER_CAN_A_ID = 16;
+   public static final int LASER_CAN_B_ID = 17;
+
+   public static class DrivebaseConstants
+   {
       public static final double MAX_SPEED = Units.feetToMeters(14.5);
       public static final double WHEEL_LOCK_TIME = 10; // seconds
 
    }
 
-   public static class OperatorConstants {
+
+   public static class OperatorConstants 
+   {
       public static final int kDriverControllerPort = 0;
 
       public static final double DEADBAND = 0.1;
       public static final double LEFT_Y_DEADBAND = 0.1;
       public static final double RIGHT_X_DEADBAND = 0.1;
       public static final double TURN_CONSTANT = 6;
+   }
+
+   public static class ArmConstants
+   {
+      public static final double WRIST_PID_P = 2.4;
+      public static final double WRIST_PID_I = 0.0;
+      public static final double WRIST_PID_D = 0.1;
+      public static final double WRIST_PID_FF = 0.0;
+
    }
 }
---
+++ ThresherBot/src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,206 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.units.measure.*;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Arm extends SubsystemBase
+{
+   // ELEVATOR
+   //private final SparkMax m_elevatorMaster = new SparkMax(Constants.ELEVATOR_MASTER_CAN_ID, MotorType.kBrushless);
+   //private final SparkMax m_elevatorSlave = new SparkMax(Constants.ELEVATOR_SLAVE_CAN_ID, MotorType.kBrushless);
+   //private final SparkClosedLoopController m_elevatorPID = m_elevatorMaster.getClosedLoopController();
+   
+   // WRIST
+   private final TalonFX m_wrist = new TalonFX(Constants.ARM_WRIST_CAN_ID);
+   private final CANcoder m_wristEncoder = new CANcoder(Constants.ARM_ENCODER_CAN_ID); 
+   private final PositionVoltage m_wristVoltage = new PositionVoltage(0);
+   //private final LaserCan m_laser = new LaserCan(Constants.LASER_CAN_A_ID);
+
+   private ShuffleboardTab tab = Shuffleboard.getTab("Arm");
+   private GenericEntry dbLaserEntry = tab.add("Laser Distance", 0).getEntry();
+   private GenericEntry dbWristAngle = tab.add("Wrist Angle", 0).getEntry();
+   private GenericEntry dbWristSetpoint = tab.add("Wrist Set", 0).getEntry();
+   private GenericEntry dbWristCurrent = tab.add("Wrist Amps", 0).getEntry();
+   private GenericEntry dbWristMotorV = tab.add("Wrist Volts", 0).getEntry();
+   /*
+   private GenericEntry sbtShooterSetpoint = tab.add("Shooter Setpt", 0).getEntry();
+   private GenericEntry sbtNoteInsideIntake = tab.add("Is Note Inside", 0).getEntry();
+   private GenericEntry sbtArmSpeed = tab.add("Arm Speed", 0).getEntry();
+   private GenericEntry sbtArmPosition = tab.add("Arm Position", 0).getEntry();
+   private GenericEntry stbArmXboxInput = tab.add("Arm Xbox Input", 0).getEntry();
+   private GenericEntry stbArmXboxOutput = tab.add("Arm Xbox Scaled", 0).getEntry();
+   private GenericEntry stbArmClosedLoopEnabled = tab.add("ArmClosedLoopEnabled", 0).getEntry();
+   private GenericEntry stbArmSetpoint = tab.add("Arm Setpoint", 0).getEntry();
+   private GenericEntry stbArmError = tab.add("Arm Error", 0).getEntry();
+
+   private GenericEntry stbShootOpenVal = tab.add("Shoot Cmd Value", 0).getEntry();
+  */
+
+
+   private final StatusSignal<Boolean> f_fusedSensorOutOfSync = m_wrist.getFault_FusedSensorOutOfSync(false);
+   private final StatusSignal<Boolean> sf_fusedSensorOutOfSync = m_wrist.getStickyFault_FusedSensorOutOfSync(false);
+   private final StatusSignal<Boolean> f_remoteSensorInvalid = m_wrist.getFault_RemoteSensorDataInvalid(false);
+   private final StatusSignal<Boolean> sf_remoteSensorInvalid = m_wrist.getStickyFault_RemoteSensorDataInvalid(false);
+
+   private final StatusSignal<Angle> fx_pos = m_wrist.getPosition(false);
+   private final StatusSignal<AngularVelocity> fx_vel = m_wrist.getVelocity(false);
+   private final StatusSignal<Angle> cc_pos = m_wristEncoder.getPosition(false);
+   private final StatusSignal<AngularVelocity> cc_vel = m_wristEncoder.getVelocity(false);
+   private final StatusSignal<Angle> fx_rotorPos = m_wrist.getRotorPosition(false);
+
+   private final DutyCycleOut m_dutyCycleControl = new DutyCycleOut(0);
+
+   private int printCount = 0;
+
+   private double m_wristSetpoint = 0.0;
+
+   public Arm()
+   {
+      ConfigureWrist();
+
+   }
+   // GUIDES: https://v6.docs.ctr-electronics.com/en/stable/docs/migration/migration-guide/closed-loop-guide.html
+   // https://github.com/CrossTheRoadElec/Phoenix6-Examples/blob/main/java/PositionClosedLoop/src/main/java/frc/robot/Robot.java
+
+   @Override
+   public void periodic()
+   {
+      // This method will be called once per scheduler run
+      ExecuteWristPIDPeriodic();
+   
+      UpdateSmartDashboard();
+
+
+   }
+
+   public void ConfigureWrist()
+   {
+      CANcoderConfiguration cc_cfg = new CANcoderConfiguration();
+      cc_cfg.MagnetSensor.withAbsoluteSensorDiscontinuityPoint(Rotations.of(0.5));
+      cc_cfg.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+      cc_cfg.MagnetSensor.withMagnetOffset(Rotations.of(0.4));
+      m_wristEncoder.getConfigurator().apply(cc_cfg);
+
+      TalonFXConfiguration fx_cfg = new TalonFXConfiguration();
+      fx_cfg.Feedback.FeedbackRemoteSensorID = m_wristEncoder.getDeviceID();
+      fx_cfg.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+      fx_cfg.Feedback.SensorToMechanismRatio = 1.0;
+      fx_cfg.Feedback.RotorToSensorRatio = 12.8;
+
+      fx_cfg.Slot0.kP = Constants.ArmConstants.WRIST_PID_P;
+      fx_cfg.Slot0.kI = Constants.ArmConstants.WRIST_PID_I;
+      fx_cfg.Slot0.kD = Constants.ArmConstants.WRIST_PID_D;
+
+      fx_cfg.Voltage.withPeakForwardVoltage(Volts.of(8))
+         .withPeakReverseVoltage(Volts.of(-2));
+
+
+      StatusCode status = m_wrist.getConfigurator().apply(fx_cfg);
+
+      if (!status.isOK())
+      {
+         System.out.println("Wrist configuration failed: " + status);
+      }
+
+      m_wrist.setPosition(0);
+   }
+
+   public void ExecuteWristPIDPeriodic()
+   {
+      if (++printCount >= 10)
+      {
+         printCount = 0;
+
+         BaseStatusSignal.refreshAll(
+         f_fusedSensorOutOfSync,
+         sf_fusedSensorOutOfSync,
+         f_remoteSensorInvalid,
+         sf_remoteSensorInvalid,
+         fx_pos, fx_vel,
+         cc_pos, cc_vel
+         );
+
+         // If any faults happen, print them out. Sticky faults will always be present if live-fault occurs
+         boolean anyFault = sf_fusedSensorOutOfSync.getValue() || sf_remoteSensorInvalid.getValue();
+         SmartDashboard.putBoolean("CANCODER FAULT?", anyFault);
+         if (anyFault)
+         {
+            System.out.println("A fault has occurred:");
+            /* If we're live, indicate live, otherwise if we're sticky indicate sticky, otherwise do nothing */
+            if (f_fusedSensorOutOfSync.getValue())
+            {
+               System.out.println("Fused sensor out of sync live-faulted");
+            } 
+            else if (sf_fusedSensorOutOfSync.getValue())
+            {
+               System.out.println("Fused sensor out of sync sticky-faulted");
+            }
+            /* If we're live, indicate live, otherwise if we're sticky indicate sticky, otherwise do nothing */
+            if (f_remoteSensorInvalid.getValue())
+            {
+               System.out.println("Missing remote sensor live-faulted");
+            } 
+            else if (sf_remoteSensorInvalid.getValue())
+            {
+               System.out.println("Missing remote sensor sticky-faulted");
+            }
+         }
+
+         //if (m_joystick.getAButton()) {
+         /* Clear sticky faults */
+         //  m_fx.clearStickyFaults();
+         //}
+
+         /* Print out current position and velocity */
+         //System.out.println("FX Position: " + fx_pos + " FX Vel: " + fx_vel);
+         //System.out.println("CC Position: " + cc_pos + " CC Vel: " + cc_vel);
+         System.out.println("");
+      }
+       
+      //m_mechanism.update(fx_rotorPos, cc_pos, fx_pos);
+   }
+
+   private void UpdateSmartDashboard()
+   {
+      dbLaserEntry.setDouble(0);
+      dbWristAngle.setDouble(m_wristEncoder.getPosition().getValueAsDouble());
+      dbWristSetpoint.setDouble(m_wristSetpoint);
+      dbWristCurrent.setDouble(m_wrist.getTorqueCurrent().getValueAsDouble());
+      dbWristMotorV.setDouble(m_wrist.getMotorVoltage().getValueAsDouble());
+
+      //sbtShooterSetpoint.setDouble(m_shooterSetpoint);
+      //sbtArmSpeed.setDouble((m_armMotor.get()));
+      //sbtArmPosition.setDouble(m_armMotor.getSelectedSensorPosition());
+      //stbArmSetpoint.setDouble(m_armSetpoint);
+      //stbArmError.setDouble(m_armMotor.getClosedLoopError());
+   }
+
+}
